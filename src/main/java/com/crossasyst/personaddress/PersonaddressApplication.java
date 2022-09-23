2f50cc858080a6d725dee75c5e834b63be2259ec
saurabh <saurabhkanawade30@gmail.com>
2022-09-23 11:18:57 +0530

    commited the code of the mapping

---
+++ src/main/java/com/crossasyst/personaddress/PersonaddressApplication.java
@@ -0,0 +1,13 @@
+package com.crossasyst.personaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaddressApplication.class, args);
+	}
+
+}
